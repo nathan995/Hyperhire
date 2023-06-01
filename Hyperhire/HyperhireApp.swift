37a1f04d4d20a1f80f677b18d90ab89fb618793b
Nathan Getachew <nathangetachew95@gmail.com>
2023-06-01 11:01:19 +0300

    Initial Commit

---
+++ Hyperhire/HyperhireApp.swift
@@ -0,0 +1,17 @@
+//
+//  HyperhireApp.swift
+//  Hyperhire
+//
+//  Created by Nathan Getachew on 6/1/23.
+//
+
+import SwiftUI
+
+@main
+struct HyperhireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
